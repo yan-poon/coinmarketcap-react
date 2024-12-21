ed0afdc3e6c28c9e0ab8e94c62e361b7c127eb1a
Cavour Poon <cavouriypoon@outlook.com>
2024-12-22 03:04:00 +0800

    Fix incorrect display for CoinQuote

--- src/components/CoinQuote.jsx
+++ src/components/CoinQuote.jsx
@@ -6,13 +6,14 @@ const CoinQuote = ({ coinQuoteData, lastUpdateTime }) => {
     if (!coinQuoteData) {
         return <p>Loading...</p>
     } else {
-        return (<div className="coin-quote">
+        const maxSupply=coinQuoteData.infinite_supply===true ? "Unlimited" : `${getFormattedIntegerString(coinQuoteData.max_supply)} ${coinQuoteData.symbol}` ;
+        return (<div>
             <h2>{coinQuoteData.name} {coinQuoteData.symbol} <span className="rank-circle">#{coinQuoteData.cmc_rank}</span><span style={{ color: getTextColor(coinQuoteData.quote.USD.percent_change_24h) }}>{getDirectionTriangle(coinQuoteData.quote.USD.percent_change_24h)} {coinQuoteData.quote.USD.percent_change_24h.toFixed(2)}% 24H</span> </h2>
             <h2>${getFormattedPrice(coinQuoteData.quote.USD.price)}</h2>
             <h3>Market cap: {getFormattedIntegerString(coinQuoteData.quote.USD.market_cap)}</h3>
             <h3>Volume (24h): {getFormattedIntegerString(coinQuoteData.quote.USD.volume_24h)}</h3>
             <h3>Total Supply: {getFormattedIntegerString(coinQuoteData.total_supply)} {coinQuoteData.symbol}</h3>
-            <h3>Max Supply: {getFormattedIntegerString(coinQuoteData.max_supply)} {coinQuoteData.symbol}</h3>
+            <h3>Max Supply: {maxSupply}</h3>
             <p>Last updated at {lastUpdateTime}</p>
         </div>)
     }
