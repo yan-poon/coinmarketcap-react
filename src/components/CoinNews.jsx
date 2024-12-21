b24ecc3407f95cae4415d93a5bc00ff091614f80
Cavour Poon <cavouriypoon@outlook.com>
2024-12-22 02:20:03 +0800

    Fix CoinNews CSS

--- src/components/CoinNews.jsx
+++ src/components/CoinNews.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { fetchCoinNewsData } from '../services/NewsApiService';
 import '../css/CoinNews.css';
 
-const CoinNews = ({coinSymbol=""}) => {
+const CoinNews = ({ coinSymbol = "" }) => {
     const [coinNewsData, setcoinNewsData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [count, setCount] = useState(20);
@@ -24,13 +24,18 @@ const CoinNews = ({coinSymbol=""}) => {
         setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : 1));
     };
 
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    };
+
     if (!coinNewsData) {
         return <p>Loading...</p>
     } else {
-        var title="Latest news of Cryptocurrency";
-        if(coinSymbol){
-            const texts=coinSymbol.split("+")
-            title=`Latest News of ${texts[0]} (${texts[1]})`;
+        var title = "Latest news of Cryptocurrency";
+        if (coinSymbol) {
+            const texts = coinSymbol.split("+")
+            title = `Latest News of ${texts[0]} (${texts[1]})`;
         }
         return (<div className="content-container">
             <h2>{title}</h2>
@@ -39,7 +44,7 @@ const CoinNews = ({coinSymbol=""}) => {
                     <li key={coinNewsData.indexOf(coinNews)} style={{ cursor: 'pointer' }} onClick={() => window.open(coinNews.url, '_blank')}>
                         <h3>{coinNews.name}</h3>
                         <p>{coinNews.description}</p>
-                        <p className="details">Click for Details</p>
+                        <p className="news-footer"><span>{formatDate(coinNews.datePublished)}</span><span className="details">Click for Details</span></p>
                     </li>))}
             </ul>
             <div>
